24fe1b36f41d2f302f8acd3e175c0af0e4f7059d
CondorDK <carn2g@gmail.com>
2016-11-18 06:43:45 -0300

    changes5

--- resources/views/pages/materiales.blade.php
+++ resources/views/pages/materiales.blade.php
@@ -8,9 +8,13 @@
 <section class="service_grids color-dark bg-gray">
     <div class="container marginbot-50">
       <div class="row">
+
+<br>
+<br>
+<br>
         <div class="col-lg-8 col-lg-offset-2">
             <div class="section-heading text-center">
-            <div class="m_3"><span class="left_line1 wow fadeInLeft" data-wow-delay="0.4s"></span> <h2 class="h-bold wow fadeIn" data-wow-delay="0.4s">Materiales</h2><span class="right_line1 wow fadeInRight" data-wow-delay="0.4s"> </span></div>
+            <div class="m_3"><span class="left_line1 wow fadeInLeft" data-wow-delay="0.4s"></span> <h1 class="h-bold wow fadeIn" data-wow-delay="0.4s">Materiales</h1><span class="right_line1 wow fadeInRight" data-wow-delay="0.4s"> </span></div>
             <div class="divider-header wow fadeInDown" data-wow-delay="0.4s"></div>
             <p class="wow fadeIn" data-wow-delay="0.4s">Materiales Disponibles Que Puedes Encontrar.</p>
          </div>
@@ -72,22 +76,10 @@
                 </div>
              </div>
           </div>
-            <div class="col-md-3 ">
-                <div class="wow fadeInLeft" data-wow-delay="0.2s">
-                  <div>
-                    <ul class="menu cf">
-                      <li>
-                        <a><img style="max-width:230px; margin-left:-5px;" src="../images/VIDRIOSolo.png" class="img-rounded" alt="imagen circular"></a>
-                        <ul class="submenu">
-                          <li><a href="tengo">Tengo</a></li>
-                          <li><a href="necesito">Necesito</a></li>
-                          <li><a href="como">¿Como?</a></li>
-                        </ul>           
-                      </li>
-                    </ul>
-                </div>
-             </div>
-          </div>
+
+
+
+
         </div>    
     </div>
 </div>
