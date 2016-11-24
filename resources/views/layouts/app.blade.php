a0cb0881f4c62cb67993d9fbb520a483bed3f593
AdR1xN <adrian.08lv@gmail.com>
2016-11-24 19:57:30 -0300

    Welcome mejorado, demas con caca aun

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,18 +2,25 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>Laravel</title>
+    <title>Vitagreen</title>
 
     <!-- Fonts -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css" integrity="sha384-XdYbMnZ/QjLh6iI4ogqCTaIjrFk87ip+ekIjefZch0Y+PvJ8CDYtEs1ipDmPorQ+" crossorigin="anonymous">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700">
+    <link href="{{{ asset('/css/font-awesome.min.css') }}}" rel="stylesheet">
 
     <!-- Styles -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
     {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
+
+    <!-- Stilos de bootstrap -->
+    <link href="{{{ asset('/css/bootstrap.min.css') }}}" rel="stylesheet">
+    <link href="{{{ asset('/css/bootstrap-responsive.min.css') }}}" rel="stylesheet">
+    <!-- Main Css -->
+    <link href="{{{ asset('/css/styles.css') }}}" rel="stylesheet">
 
     <style>
         body {
@@ -25,10 +32,10 @@
         }
     </style>
 </head>
-<body id="app-layout">
-    <nav class="navbar navbar-default navbar-static-top">
+<section class="main-nav">
+    <nav class="navbar">
         <div class="container">
-            <div class="navbar-header">
+            <div class="navbar-inner">
 
                 <!-- Collapsed Hamburger -->
                 <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
@@ -37,22 +44,12 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-
-                <!-- Branding Image -->
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    Laravel
-                </a>
-            </div>
-
-            <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                <!-- Left Side Of Navbar -->
-                <ul class="nav navbar-nav">
-                    <li><a href="{{ url('/home') }}">Home</a></li>
-                </ul>
-
-                <!-- Right Side Of Navbar -->
+                  <img src="images/vita.png" alt="logo" width="110" height="110">
+                </a></ul>
+                <!-- <Menus y mierdas> -->
                 <ul class="nav navbar-nav navbar-right">
-                    <!-- Authentication Links -->
+
                     @if (Auth::guest())
                         <li><span class="list-circle"></span> <a href="{{ url('/login') }}">Ingresar</a></li>
                         <li><span class="list-circle"></span> <a href="{{ url('/register') }}">Registrarse</a></li>
@@ -70,9 +67,13 @@
                         </li>
                     @endif
                 </ul>
-            </div>
+            </div><!-- End Navbar Collapse -->
+          <!--fin de navbar Inner -->
         </div>
-    </nav>
+      </nav>
+    </section>
+
+
 
     @yield('content')
 
@@ -80,5 +81,20 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js" integrity="sha384-I6F5OKECLVtK/BL+8iSLDEHowSAfUo76ZL9+kGAgTRdiByINKJaqTPH/QVNS1VDb" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
     {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
-</body>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/libraries/plugins.js"></script>
+  <script src="js/main.js"></script>
+
+<section class="subfooter">
+  <div class="container">
+    <div class="row">
+      <center>
+        <div class="span12">
+          <p class="rights">Copyright © 2016 Vitagreen Team. Todos los derechos reservados.</p>
+        </div>
+      </center>
+    </div> <!-- end row -->
+  </div> <!-- end container -->
+</section> <!-- end subfooter -->
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,17 +1,150 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<section class="flexslider-container container">
     <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading">Welcome</div>
-
-                <div class="panel-body">
-                    Your Application's Landing Page.
+      <div class="span12">
+        <div class="flexslider" id="flex1">
+          <div class="flex-viewport" style="overflow: hidden; position: relative; height: 0px;"><ul class="slides" style="width: 1000%; transition-duration: 1s; transform: translate3d(-2276px, 0px, 0px);">
+            <li class="clone" style="width: 1138px; float: left; display: block;">
+              <img src="images/slides1.png" alt="img">
+                <!--<div class="captions">
+                  <p class="description">Biscuit gummies pudding cookie wypas macaroon.</p>
+                    </div>  end captions -->
+                    </li>
+                    <li class="clone" style="width: 1138px; float: left; display: block;">
+                      <img src="images/slides2.png" alt="img">
+                      <!--<div class="captions">
+                          <p class="description">Biscuit gummies pudding cookie wypas macaroon.</p>
+                      </div>  end captions -->
+                    </li>
+                    <li style="width: 1138px; float: left; display: block;" class="flex-active-slide">
+                      <img src="images/slides3.png" alt="img">
+                      <!--<div class="captions">
+                          <p class="description">Biscuit gummies pudding cookie wypas macaroon.</p>
+                      </div>  end captions -->
+                    </li>
+                    <li style="width: 1138px; float: left; display: block;" class="">
+                      <img src="images/slides4.png" alt="img">
+                      <!--<div class="captions">
+                          <p class="description">Biscuit gummies pudding cookie wypas macaroon.</p>
+                      </div>  end captions -->
+                    </li>
+                  </ul>
                 </div>
             </div>
+         </div> <!-- end span12 -->
+      </div> <!-- end row -->
+    </section> <!-- end flexslider-container container -->
+
+
+
+
+    <!-- ============================  Main Tagline Start ============================ -->
+    <section class="main-tagline">
+      <div class="container">
+        <div class="row">
+          <div class="span12">
+            <h1 class="title center">¡HOLA, Somos VitaGreen!</h1>
+            <h6 class="subtitle center">¡Pasa y conocé nuestros servicios. Participa en nuestra comunidad!</h6>
+          </div> <!-- end span12 -->
+        </div> <!-- end row -->
+      </div> <!-- end container -->
+    </section> <!-- end main-tagline -->
+    <!-- ============================  Main Tagline Ends ============================ -->
+
+    <!-- ============================  Title Separator Starts ============================ -->
+
+    <section class="title-separator">
+      <div class="container">
+        <div class="row">
+          <div class="span12">
+            <h5 class="title center"><span>Descubrí nuestros servicios</span></h5>
+          </div> <!-- end span12 -->
+        </div> <!-- end row -->
+      </div> <!-- end container -->
+    </section> <!-- end title-separator -->
+
+    <!-- ============================  Title Separator Ends ============================ -->
+
+
+    <!-- ============================  Features Start ============================ -->
+
+    <section class="features-container">
+      <div class="container">
+        <div class="row">
+          <ul class="unstyled feature-list">
+            <li class="feature-items">
+              <div class="feature center rocket">
+                <img src="images/rocket.png" alt="rocket">
+
+                <div class="feature-content">
+                  <h5 class="title"><span>Recicla Metal</span></h5>
+                  <p class="content">Applicake pudding faworki halvaroon icing sugar plum cake powderi  jujubes pastry </p>
+                </div> <!-- end feature-content -->
+              </div> <!-- end feature -->
+            </li>
+            <li class="feature-items v2">
+              <img src="images/line-separator.png" alt="img">
+            </li>
+            <li class="feature-items">
+              <div class="feature center camera">
+                <img src="images/camera.png" alt="rocket">
+
+                <div class="feature-content">
+                  <h5 class="title"><span>Recicla Papel</span></h5>
+                  <p class="content">Cada tonelada de papel reciclado equivale a no talar aproximadamente 20 árboles. Esos 20 árboles salvados absorben un total de 110 kilos de dióxido de carbono por año. </p>
+                </div> <!-- end feature-content -->
+              </div> <!-- end feature -->
+            </li>
+            <li class="feature-items v2">
+              <img src="images/line-separator.png" alt="img">
+            </li>
+            <li class="feature-items">
+              <div class="feature center graduate">
+                <img src="images/graduate.png" alt="rocket">
+
+                <div class="feature-content">
+                  <h5 class="title"><span>Reclcilar Plastico</span></h5>
+                  <p class="content">El plástico desechado que va a ríos, lagunas y océanos, mata alrededor de 1 millón de animales por año. Una botella de plástico puede tardar hasta 1000 años de biodegradarse.</p>
+                </div> <!-- end feature-content -->
+              </div> <!-- end feature -->
+            </li>
+            <li class="feature-items v2">
+              <img src="images/line-separator.png" alt="img">
+            </li>
+            <li class="feature-items">
+              <div class="feature center settings">
+                <img src="images/settings.png" alt="rocket">
+
+                <div class="feature-content">
+                  <h5 class="title"><span>Reciclar Vidrio</span></h5>
+                  <p class="content">El vidrio es 100% reciclable y puede ser reciclado ilimitadas veces. Este proceso consume sólo el 25% de la energía necesaria para hacer vidrio nuevo. </p>
+                </div> <!-- end feature-content -->
+              </div> <!-- end feature -->
+            </li>
+          </ul>
+        </div> <!-- end row -->
+      </div> <!-- end container -->
+    </section> <!-- end features-container -->
+
+    <!-- ============================  Features Ends ============================ -->
+
+
+     <!-- ============================  Title Separator Starts ============================ -->
+
+     <section class="title-separator">
+      <div class="container">
+        <div class="row">
+          <div class="span12">
+            <h5 class="title center"><span>Ultimos Posts</span></h5>
+          </div>
         </div>
-    </div>
-</div>
+      </div>
+    </section>
+    
 @endsection
+    <!-- ============================  Title Separator Ends ============================ -->
+
+
+    <!-- ============================  Recent Posts Start ============================ -->
